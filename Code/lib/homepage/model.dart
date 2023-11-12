d777748dad05ce92e7c68201dd294614cff2c296
Manbendra Satpathy <92919625+Manbendra2014@users.noreply.github.com>
2023-11-12 21:21:17 +0530

    Delete Code directory

--- Code/lib/homepage/model.dart
+++
@@ -1,9 +0,0 @@
-
-class MyList {
-   String title;
-   String subtitle;
-   String money;
-   bool isgood;
-
-  MyList({required this.title,required this.subtitle,required this.money,required this.isgood});
-}
